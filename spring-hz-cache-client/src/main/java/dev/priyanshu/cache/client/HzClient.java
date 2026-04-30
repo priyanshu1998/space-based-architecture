04745046abc3cc535452f46358bcca3bcfb5d0c6
priyanshu1998 <priyanshutirkey1998@gmail.com>
2026-05-01 04:07:37 +0530

    implement publish-subscribe functionality using Hazelcast topics

--- spring-hz-cache-client/src/main/java/dev/priyanshu/cache/client/HzClient.java
+++ spring-hz-cache-client/src/main/java/dev/priyanshu/cache/client/HzClient.java
@@ -2,6 +2,7 @@ package dev.priyanshu.cache.client;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.topic.ITopic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,38 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class HzClient implements CommandLineRunner {
+
+    private static final HazelcastInstance hz;
+
+    static {
+        hz = HazelcastClient.newHazelcastClient();
+    }
+
+    private static class Subject {
+        public void publish(String topicName, String msg) {
+            var topic = hz.getTopic(topicName);
+            topic.publish(msg);
+        }
+    }
+
+    private static class Observer {
+        public void subscribe(String topicName) {
+            var topic = hz.getTopic(topicName);
+            topic.addMessageListener(message -> log.info("Received message :: {}", message.getMessageObject()));
+        }
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        HazelcastInstance hz = HazelcastClient.newHazelcastClient();
-
         log.info("hz {} group name :: {}", hz.getName(), hz.getConfig().getClusterName());
+
+        if(args.length > 0 && args[0].equalsIgnoreCase("publish")){
+            new Subject().publish("my-topic", "Hello from client");
+        } else {
+            new Observer().subscribe("my-topic");
+        }
     }
+
+
+
 }
